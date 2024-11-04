4f237ed493dd9cced63c1b5eacede45a0c476975
aenia12 <aenia12@gmail.com>
2024-11-04 15:35:54 -0500

    Working on app.js and app.css.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,17 +7,72 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My name is Aenia Amin, and I am a junior front-end developer based in
+          the United States. I use VS Code, GitHub, and Netlify, and am
+          PC-based. I know these technologies: JavaScript, React.js, Bootstrap,
+          and Responsive development.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/aenia-amin-66b45684/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Linkedin Portfolio
+        </a>
+        <a
+          className="Resume-link"
+          href="https://www.linkedin.com/in/aenia-amin-66b45684/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Resume
+        </a>
+        <a
+          className="Project-link"
+          href="https://www.shecodes.io/graduates/77665-aenia-amin"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Personal Projects
+        </a>
+        <a
+          className="Contact-link"
+          href="mailto:aenia12@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Email me at aenia12@gmail.com
         </a>
       </header>
+      <footer className="App-footer">
+        <small>
+          This project is coded by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/77665-aenia-amin"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Aenia Amin
+          </a>{" "}
+          and is hosted on{" "}
+          <a
+            href="https://github.com/aenia12/next.aminportfolio"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub{" "}
+          </a>
+          and{" "}
+          <a
+            href="https://shecodesnextaminportfolio.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify.
+          </a>
+        </small>
+      </footer>
     </div>
   );
 }
